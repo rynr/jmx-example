af092a74122103289fd4689407927df9f358e359
Rainer Jung <rainer.jung@friendscout24.de>
2013-10-23 09:17:32 +0200

    Added Notifications

--- src/main/java/de/friendscout24/monitor/Monitor.java
+++ src/main/java/de/friendscout24/monitor/Monitor.java
@@ -5,21 +5,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.management.AttributeChangeNotification;
 import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanNotificationInfo;
 import javax.management.MBeanRegistrationException;
 import javax.management.MalformedObjectNameException;
 import javax.management.NotCompliantMBeanException;
+import javax.management.NotificationBroadcasterSupport;
 import javax.management.ObjectName;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Monitor implements MonitorMBean {
+public class Monitor extends NotificationBroadcasterSupport implements MonitorMBean {
 
 	private static final String MBEAN_NAME = "de.friendscout24.monitor:type=Monitor";
 	boolean active;
 	Map<String, AtomicInteger> counter;
 	private Logger logger;
+	private int sequenceNumber;
 
 	private Monitor() {
 		logger = LoggerFactory.getLogger(getClass());
@@ -51,7 +55,7 @@ public class Monitor implements MonitorMBean {
 		if (active) {
 			if (counter.containsKey(name)) {
 				logger.debug("Increasing counter " + name);
-				counter.get(name).incrementAndGet();
+				sendNotification(name, counter.get(name).incrementAndGet());
 			} else {
 				logger.debug("Increasing (new) counter " + name);
 				counter.put(name, new AtomicInteger(1));
@@ -59,6 +63,11 @@ public class Monitor implements MonitorMBean {
 		} else {
 			logger.info("Did not increment counter " + name + " (inactive)");
 		}
+	}
+
+	private void sendNotification(String name, int new_counter) {
+		sendNotification(new AttributeChangeNotification(this, sequenceNumber++, System.currentTimeMillis(),
+				"Counter \"" + name + "\" incremented", "Counter", "int", new_counter - 1, new_counter));
 	}
 
 	@Override
@@ -79,4 +88,12 @@ public class Monitor implements MonitorMBean {
 		}
 	}
 
+	@Override
+	public MBeanNotificationInfo[] getNotificationInfo() {
+		String[] types = new String[] { AttributeChangeNotification.ATTRIBUTE_CHANGE };
+		String name = AttributeChangeNotification.class.getName();
+		String description = "An attribute of this MBean has changed";
+		MBeanNotificationInfo info = new MBeanNotificationInfo(types, name, description);
+		return new MBeanNotificationInfo[] { info };
+	}
 }
